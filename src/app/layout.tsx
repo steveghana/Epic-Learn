efc26f3f2ba0f0872b3e0e62ddb5df27e3e21775
steveghana <boatengstephen707@gmail.com>
2024-01-17 14:03:36 +0000

    Automated Commit file: _update src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import { Header } from "./header";
 import { Footer } from "./footer";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
+import { ClerkProvider } from "@clerk/nextjs";
+import { QueryClientProvider } from "@tanstack/react-query";
+import Providers, { ConvexClientProvider } from "@/components/Providers";
 
 // const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -13,22 +16,26 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        {/* <ClerkProvider
-          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
-        > */}
-        {/* <ConvexProviderWithClerk client={convex} useAuth={useAuth}> */}
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          disableTransitionOnChange
-        >
-          <Header />
-          {children}
-          <Footer />
-          <Toaster />
-        </ThemeProvider>
-        {/* </ConvexProviderWithClerk> */}
-        {/* </ClerkProvider> */}
+        <Providers>
+          <ClerkProvider
+            publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+          >
+            <ConvexClientProvider>
+              {/* <ConvexProviderWithClerk client={convex} useAuth={useAuth}> */}
+              <ThemeProvider
+                attribute="class"
+                defaultTheme="light"
+                disableTransitionOnChange
+              >
+                {/* <Header /> */}
+                {children}
+                <Footer />
+                <Toaster />
+              </ThemeProvider>
+            </ConvexClientProvider>
+            {/* </ConvexProviderWithClerk> */}
+          </ClerkProvider>
+        </Providers>
       </body>
     </html>
   );
