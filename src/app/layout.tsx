b315f60ff2710adde0c980fe74f232d2fd19609e
steveghana <boatengstephen707@gmail.com>
2024-01-18 11:37:40 +0000

    Automated Commit file: _update src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
               >
                 {/* <Header /> */}
                 {children}
-                <Footer />
+                {/* <Footer /> */}
                 <Toaster />
               </ThemeProvider>
             </ConvexClientProvider>
